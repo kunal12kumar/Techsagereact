484ccfa9f3cb9408397e694e602e291ecb43db2c
kunal12kumar <23cs3035@rgipt.ac.in>
2025-01-04 01:42:58 +0530

    adding login  logout and authenticated , callback

--- techsagefrontend/src/index.js
+++ techsagefrontend/src/index.js
@@ -2,31 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import ContactUs from './pages/ContactUs';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let allrouter=createBrowserRouter([
+let allrouter = createBrowserRouter([
   {
-    path:'/',
-    element:<Home></Home>
+    path: '/',
+    element: <Home></Home>
   },
   {
-    path:'/signup',
-    element:<Signup></Signup>
+    path: '/signup',
+    element: <Signup></Signup>
   },
   {
-    path:'/contactus',
-    element:<ContactUs></ContactUs>
+    path: '/contactus',
+    element: <ContactUs></ContactUs>
   },
-  
+
 
 ])
 root.render(
   <React.StrictMode>
-    <RouterProvider router={allrouter}></RouterProvider>
+    <Auth0Provider
+      domain="dev-xufegyqmkqi8ezy1.us.auth0.com"
+      clientId="s0ymz0pMwIHrQRKBgyJ0qDs54jIAS5DX"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <RouterProvider router={allrouter}></RouterProvider>
+
+    </Auth0Provider>,
+
   </React.StrictMode>
 );
 
